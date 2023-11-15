0bfd04fc72b237de30bf346bec8054684ce7aad0
BearlyFunctional <hjbell123.hb@gmail.com>
2023-11-15 13:34:55 -0800

    Added code to index.js for generation of readme file.

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
 
-// TODO: Create an array of questions for user input
-const questions = [];
+inquirer .prompt([
+    {name: 'title', message: "What is your project's title?",},
+    {name: 'description', message: "Please enter your project's desription"},
+    {name: 'installInst', message: 'How is your project installed?'},
+    {name: 'usageInst', message: 'How is your project used?'},
+    {name: 'contributionGuides', message: 'Please enter your contribution guidelines:'},
+    {name: 'tests', message: 'Please enter any test instructions for your project:'},
+    {name: 'projectLicense', message: 'What license does your project use?'},
+    {name: 'projectQuestions', message: 'How can users reach you to as questions?'}
+]) .then ((response) => {
+    generateReadmeText(response)
+})
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function generateReadmeText (responseData) {
+    const {title, description, installInst, usageInst, contributionGuides, tests, projectLicense, projectQuestions} = responseData
+    let readmeTableOfContents = ''
+    let generatedReadmeBody = ''
+    if (description != '') {
+        generatedReadmeBody += `\n\n##Description\n${description}`
+        readmeTableOfContents += '\n- [Description](#description)'
+    }
+    if (installInst != '') {
+        generatedReadmeBody += `\n\n##Install\n${installInst}`
+        readmeTableOfContents += '\n- [Install](#install)'
+    }
+    if (usageInst != '') {
+        generatedReadmeBody += `\n\n##Usage\n${usageInst}`
+        readmeTableOfContents += '\n- [Usage](#usage)'
+    }
+    if (contributionGuides != '') {
+        generatedReadmeBody += `\n\n##Contribution\n${contributionGuides}`
+        readmeTableOfContents += '\n- [Contribution](#contribution)'
+    }
+    if (tests != '') {
+        generatedReadmeBody += `\n\n##Tests\n${tests}`
+        readmeTableOfContents += '\n- [Tests](#tests)'
+    }
+    if (projectLicense != '') {
+        generatedReadmeBody += `\n\n##License\n${projectLicense}`
+        readmeTableOfContents += '\n- [License](#license)'
+    }
+    if (projectQuestions != '') {
+        generatedReadmeBody += `\n\n##Questions?\n${projectQuestions}`
+        readmeTableOfContents += '\n- [Questions?](#questions?)'
+    }
+    let generatedReadme = `#${title}\n\n##Table of Contents${readmeTableOfContents}` + generatedReadmeBody
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+    fs.writeFile('./output/README.md', generatedReadme, err => {
+        if (err) {
+          console.error(err);
+        } else {
+            console.log('Readme file created! Location is ./output/README.md')
+        }
+      })
+}
